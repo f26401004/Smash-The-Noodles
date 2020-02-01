0b96ac532d3737ea1ec7b47e71ff02ae76466772
刁彥斌 <tigerpaws@diaoyanbinde-MacBook-Pro.local>
2020-02-01 13:13:40 +0800

    realtime multiplayer

--- Assets/Scripts/Socket.cs
+++ Assets/Scripts/Socket.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Nakama;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Socket : MonoBehaviour {
     private ISession session;
@@ -10,12 +11,13 @@ public class Socket : MonoBehaviour {
     private ISocket socket;
     private string matchId;
     private int memberNumber;
+    public InputField id;
     // Start is called before the first frame update
     async void Start () {
         // config the member number of a game
         this.memberNumber = 2;
 
-        var client = new Client ("http", "2c49e37d.ngrok.io", 80, "defaultkey");
+        var client = new Client ("http", "b6901bd0.ngrok.io", 80, "defaultkey");
         // get the current device id
         this.deviceId = PlayerPrefs.GetString ("nakama.deviceid");
         if (string.IsNullOrEmpty (this.deviceId)) {
@@ -32,13 +34,40 @@ public class Socket : MonoBehaviour {
         this.socket.Closed += () => {
             Debug.Log ("Socket closed.");
         };
+        this.socket.ReceivedMatchState += newState => {
+            var enc = System.Text.Encoding.UTF8;
+            var content = enc.GetString(newState.State);
+            Debug.LogFormat("Received: {0}, {1}", newState.OpCode, content);
+        };
         await this.socket.ConnectAsync (this.session);
 
         // test send message
-        await this.JoinMatch ();
+        // await this.JoinMatch ();
+		
     }
 
-    async Task JoinMatch () {
+	public async void CreateMatch()
+	{
+        var match = await socket.CreateMatchAsync();
+        Debug.LogFormat("New match with id '{0}'.", match.Id);
+        JoinMatch(match.Id);
+    }
+
+    public async void JoinMatch()
+	{
+        var matchId = id.text;
+        var match = await socket.JoinMatchAsync(matchId);
+        await socket.SendMatchStateAsync(matchId, 1, "{\"hello\":\"world\"}");
+    }
+
+    public async void JoinMatch(string matchId)
+    {
+        var match = await socket.JoinMatchAsync(matchId);
+        id.text = matchId;
+        await socket.SendMatchStateAsync(matchId, 1, "{\"hello\":\"world\"}");
+    }
+
+    /*async Task JoinMatch () {
         var query = "*";
         var minCount = this.memberNumber;
         var maxCount = this.memberNumber;
@@ -59,7 +88,7 @@ public class Socket : MonoBehaviour {
             var content = enc.GetString (newState.State);
             Debug.LogFormat ("Received: {0}, {1}", newState.OpCode, content);
         };
-    }
+    }*/
 
     void sendMessage () {
         socket.SendMatchStateAsync (this.matchId, 1, "{\"hello\":\"world\"}");
