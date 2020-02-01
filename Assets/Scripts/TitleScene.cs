8e63242428d0c865ea280904a1b56c74addcb055
Jack Huang <f26401004@gmail.com>
2020-02-02 03:23:43 +0800

    Complete the basic title scene

--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -1,27 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleScene : MonoBehaviour {
-    public GameObject startButton;
-    public GameObject exitButton;
-    private Animator startAnimator;
-    private Animator exitAnimator;
+    // public GameObject startButton;
+    // public GameObject exitButton;
+    // private Animator startAnimator;
+    // private Animator exitAnimator;
     private int currentIndex;
     // Start is called before the first frame update
     void Start () {
-        startAnimator = startButton.GetComponent<Animator> ();
-        endAnimator = endButton.GetComponent<Animator> ();
+        // startAnimator = startButton.GetComponent<Animator> ();
+        // exitAnimator = exitButton.GetComponent<Animator> ();
         currentIndex = 0;
     }
 
     // Update is called once per frame
     void Update () {
-        if (Input.GetKeyDown (KeyCode.Up)) {
-            currentIndex = (currentIndex + 3) % 2
+        if (Input.GetKeyDown (KeyCode.UpArrow)) {
+            currentIndex = (currentIndex + 3) % 2;
         }
-        if (Input.GetKeyDown (KeyCode.Down)) {
-            currentIndex = (currentIndex + 1) % 2
+        if (Input.GetKeyDown (KeyCode.DownArrow)) {
+            currentIndex = (currentIndex + 1) % 2;
         }
+    }
+    public void enterGame () {
+        SceneManager.LoadScene ("level0");
+    }
+    public void exitGame () {
+        Application.Quit ();
     }
 }
