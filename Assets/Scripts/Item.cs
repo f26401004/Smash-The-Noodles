7631392952eef71a450b7655ffc1da1e61df2f2b
刁彥斌 <tigerpaws@diaoyanbinde-MacBook-Pro.local>
2020-02-01 17:21:21 +0800

    add item

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string key;
+	public enum ItemType
+	{
+		Pizza, Suasage, Noodles, Glue, Paint
+	}
+
+	public ItemType type;
+}
